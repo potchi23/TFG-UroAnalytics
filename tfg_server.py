c72ed00e079e57996b3da056f7f827a2ce23f55d
Richard Correa <53357059+potchi23@users.noreply.github.com>
2021-12-02 18:26:48 +0100

    Initial commit
    Base del server

---
+++ tfg_server.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, session, redirect
+from urllib import parse
+import requests
+# Resto de importaciones
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return "Hello World!", 200
+     
+class FlaskConfig:
+    '''Configuración de Flask'''
+    # Activa depurador y recarga automáticamente
+    ENV = 'development'
+    DEBUG = True
+    TEST = True
+    # Imprescindible para usar sesiones
+    SECRET_KEY = 'clave secreta que no se si habra que cambiar'
+    STATIC_FOLDER = 'static'
+    TEMPLATES_FOLDER = 'templates'
+
+
+if __name__ == '__main__':
+    app.config.from_object(FlaskConfig())
+    app.run()
