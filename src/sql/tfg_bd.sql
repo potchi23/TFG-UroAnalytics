cdfde36aff11dbebb332edc7af25e82b4f650f9e
Laura González Falque <76649387+laurafalque14@users.noreply.github.com>
2021-12-06 17:22:46 +0100

    Add files via upload

--- src/sql/tfg_bd.sql
+++ src/sql/tfg_bd.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Dec 04, 2021 at 01:59 PM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 8.0.13
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 06-12-2021 a las 17:10:24
+-- Versión del servidor: 10.1.36-MariaDB
+-- Versión de PHP: 7.2.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -24,7 +25,6 @@ CREATE DATABASE IF NOT EXISTS `phpmyadmin` DEFAULT CHARACTER SET utf8 COLLATE ut
 USE `phpmyadmin`;
 
 -- --------------------------------------------------------
-
 --
 -- Database: `tfg_db`
 --
@@ -34,7 +34,38 @@ USE `tfg_db`;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Estructura de tabla para la tabla `pacientes`
+--
+
+CREATE TABLE `pacientes` (
+  `id_paciente` int(10) UNSIGNED NOT NULL,
+  `nombre` varchar(20) NOT NULL,
+  `apellido1` varchar(20) NOT NULL,
+  `apellido2` varchar(20) NOT NULL,
+  `patologia` varchar(100) NOT NULL,
+  `id_doctor` int(10) UNSIGNED NOT NULL,
+  `indices` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `prediciones`
+--
+
+CREATE TABLE `prediciones` (
+  `id_predic` int(10) UNSIGNED NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `id_paciente` int(10) UNSIGNED NOT NULL,
+  `indices` varchar(500) NOT NULL,
+  `fecha` date NOT NULL,
+  `informe` varchar(10000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
 --
 
 CREATE TABLE `users` (
@@ -45,11 +76,11 @@ CREATE TABLE `users` (
   `email` varchar(50) NOT NULL,
   `password` varchar(30) NOT NULL,
   `type` varchar(5) NOT NULL DEFAULT 'user',
-  `accepted` tinyint(1) NOT NULL DEFAULT 0
+  `accepted` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `users`
+-- Volcado de datos para la tabla `users`
 --
 
 INSERT INTO `users` (`id`, `name`, `surname_1`, `surname_2`, `email`, `password`, `type`, `accepted`) VALUES
@@ -57,25 +88,55 @@ INSERT INTO `users` (`id`, `name`, `surname_1`, `surname_2`, `email`, `password`
 (9, 'Pepe', 'Manolito', 'Pepito', 'pepe@ucm.es', '$2y$10$LzKnjC44vrYBkn0H7gQ4Auh', 'user', 0);
 
 --
--- Indexes for dumped tables
+-- Índices para tablas volcadas
 --
 
 --
--- Indexes for table `users`
+-- Indices de la tabla `pacientes`
+--
+ALTER TABLE `pacientes`
+  ADD PRIMARY KEY (`id_paciente`),
+  ADD KEY `id_doctor` (`id_doctor`);
+
+--
+-- Indices de la tabla `prediciones`
+--
+ALTER TABLE `prediciones`
+  ADD PRIMARY KEY (`id_predic`),
+  ADD KEY `id_paciente` (`id_paciente`);
+
+--
+-- Indices de la tabla `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email` (`email`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `pacientes`
+--
+ALTER TABLE `pacientes`
+  ADD CONSTRAINT `fk_pacientes` FOREIGN KEY (`id_doctor`) REFERENCES `users` (`id`);
+
+--
+-- Filtros para la tabla `prediciones`
+--
+ALTER TABLE `prediciones`
+  ADD CONSTRAINT `fk_predicciones` FOREIGN KEY (`id_paciente`) REFERENCES `pacientes` (`id_paciente`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
