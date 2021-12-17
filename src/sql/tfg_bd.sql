cd14db39bb0dbd6cfc361ed48a0ddf464b2b89b8
Richard Correa <53357059+potchi23@users.noreply.github.com>
2021-12-17 14:32:12 +0100

    Update tfg_bd.sql

--- src/sql/tfg_bd.sql
+++ src/sql/tfg_bd.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 06-12-2021 a las 17:10:24
--- Versión del servidor: 10.1.36-MariaDB
--- Versión de PHP: 7.2.10
+-- Host: 127.0.0.1
+-- Generation Time: Dec 17, 2021 at 02:31 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.0.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,22 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `phpmyadmin`
---
-CREATE DATABASE IF NOT EXISTS `phpmyadmin` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
-USE `phpmyadmin`;
-
--- --------------------------------------------------------
---
 -- Database: `tfg_bd`
 --
-CREATE DATABASE IF NOT EXISTS `tfg_bd` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `tfg_bd`;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `pacientes`
+-- Table structure for table `pacientes`
 --
 
 CREATE TABLE `pacientes` (
@@ -50,7 +40,7 @@ CREATE TABLE `pacientes` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `prediciones`
+-- Table structure for table `prediciones`
 --
 
 CREATE TABLE `prediciones` (
@@ -65,7 +55,7 @@ CREATE TABLE `prediciones` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `users`
+-- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
@@ -76,64 +66,75 @@ CREATE TABLE `users` (
   `email` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
   `type` varchar(5) NOT NULL DEFAULT 'user',
-  `accepted` tinyint(1) NOT NULL DEFAULT '0'
+  `accepted` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Volcado de datos para la tabla `users`
+-- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `name`, `surname_1`, `surname_2`, `email`, `password`, `type`, `accepted`) VALUES
-(1, 'Richard', 'Correa', 'Mercado', 'admin@ucm.es', 'admin', 'admin', 1),
-(9, 'Pepe', 'Manolito', 'Pepito', 'pepe@ucm.es', '$2y$10$LzKnjC44vrYBkn0H7gQ4Auh', 'user', 0);
+(9, 'Pepe', 'Manolito', 'Pepito', 'pepe@ucm.es', '$2y$10$LzKnjC44vrYBkn0H7gQ4Auh', 'user', 0),
+(10, 'Richard', 'Correa', 'Mercado', 'admin@admin.es', '$2y$10$.VNItdxIcM32r.pnGBuYy.p', 'user', 0),
+(19, 'Test2', 'Test', 'Valido', 'test@valido.com', '$2y$10$wBpiPcQm5GGR5Ua4wcFEyeyVxNaz5.rxiokYTdWxTo4gjqe3q/I1W', 'user', 0),
+(21, 'Richard Junior', 'Correa', 'Mercado', 'richardcorrea306@gmail.com', '$2y$10$YdL4P7d9lNfzzgdY.xFdRuTXwRujGzWqEm4NrqIQbSUD4CCahENLu', 'user', 0),
+(25, 'Richard Junior d', 'Correa', 'Mercado', 'rirea2306@gmail.com', '$2y$10$PQ.nUBVdwJtRHxvJRb2BruT8Yu00b7qMsZ1WXneNSlLmUBH0dnkke', 'user', 0),
+(27, 'Richard Junior d', 'Correa', 'Mercado', 'richardca2306@gmail.com', '$2y$10$9pXPTGtykPA..BPgORWj4uhlgqkI6pQA7FgQfIDxRxzEtxBvyyOie', 'user', 0),
+(45, 'Richard', 'Test', 'werwer', 'ricrea2306@gmail.com', '$2y$10$eCqdPw7X9xFpLL8aNTqUO.B5yVMWQQnsJg5oqmn7vrrDYxfmIP5cO', 'user', 0),
+(47, 'Richard', 'Test', 'Test', 'richardcorrea2306@gmail.com', '$2y$10$TrIKileYQgwiTUBwU0YQf..5D2aYIeXFfm7qUCIeSv8SqjGybpPb.', 'user', 0),
+(68, 'Richard', 'Test', '', 'richard@gmail.com', '$2y$10$HNxIxvTcmBe/H4ZRkpMJMeTg0Bk2pwQUrjOm5TyN7v5yJtcQuKhU6', 'user', 1),
+(69, 'Richard', 'Test', 'werwerw', 'richardcorrea2306@yahoo.com', '$2y$10$qdwFfpCE1SxLZGzh9GazoOJwyvwaMbouXSDUUmkn.GOmlWzhUV1Em', 'user', 0),
+(73, 'Richard', 'Test', '', 'richa@gmail.com', '$2y$10$qjYcrY7ve.2rOFz22d3UoO7PAdcFbZX5bieNSHRgdpMKjsu2aT0ve', 'user', 0),
+(74, 'Pedro', 'Pedro', 'Pedro', 'pedro@ucm.es', '$2b$12$.9Woy37gzrBkG/CCTE/erekEZ1VS177TDI.tyMdh9fEQn4dJbESx2', 'admin', 1),
+(78, 'User', 'No Admin', 'admin', 'user@ucm.es', '$2b$12$X6Z0KKFDnqsRAYLPdkzJvOxrF0fsu635LBUMol.l/DFOGWtesqNjO', 'user', 0);
 
 --
--- Índices para tablas volcadas
+-- Indexes for dumped tables
 --
 
 --
--- Indices de la tabla `pacientes`
+-- Indexes for table `pacientes`
 --
 ALTER TABLE `pacientes`
   ADD PRIMARY KEY (`id_paciente`),
   ADD KEY `id_doctor` (`id_doctor`);
 
 --
--- Indices de la tabla `prediciones`
+-- Indexes for table `prediciones`
 --
 ALTER TABLE `prediciones`
   ADD PRIMARY KEY (`id_predic`),
   ADD KEY `id_paciente` (`id_paciente`);
 
 --
--- Indices de la tabla `users`
+-- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email` (`email`);
 
 --
--- AUTO_INCREMENT de las tablas volcadas
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT de la tabla `users`
+-- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=80;
 
 --
--- Restricciones para tablas volcadas
+-- Constraints for dumped tables
 --
 
 --
--- Filtros para la tabla `pacientes`
+-- Constraints for table `pacientes`
 --
 ALTER TABLE `pacientes`
   ADD CONSTRAINT `fk_pacientes` FOREIGN KEY (`id_doctor`) REFERENCES `users` (`id`);
 
 --
--- Filtros para la tabla `prediciones`
+-- Constraints for table `prediciones`
 --
 ALTER TABLE `prediciones`
   ADD CONSTRAINT `fk_predicciones` FOREIGN KEY (`id_paciente`) REFERENCES `pacientes` (`id_paciente`);
