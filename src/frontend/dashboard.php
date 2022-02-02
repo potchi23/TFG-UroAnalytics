4cf3eb073f81c7c0eb4a758dab9b13f12f6fbd04
Richard Correa <53357059+potchi23@users.noreply.github.com>
2022-02-02 09:28:34 +0100

    Prueba de graficas en dashboard

--- src/frontend/dashboard.php
+++ src/frontend/dashboard.php
@@ -15,11 +15,13 @@
         <title>Dashboard</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href="css/forms.css"/>
+        <link rel="stylesheet" href="css/dashboard.css"/>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
         <meta charset="utf-8">
     </head>
     <body>        
         <?php include_once("header/navbar.php");?>
-        <div>
+        <div class="dashboard">
             <h2>Hola, <?php echo $user->get_full_name();?></h2>
             <h3>Tipo usuario: <?php echo $user->get_type()?></h3>
             <?php
@@ -30,7 +32,14 @@
             <a href="userProfile.php">Ver mi perfil</a>
 
             <p>Falta meter más cosas y barra lateral izquierdo</p>
+            
+            <div class="graphics">
+                <div class="chart-container">
+                    <canvas id="bar-chart" width="50" height="30"></canvas>
+                </div>
+            </div>
         </div>
-
     </body>
+    
+    <script src="js/barGraphExample.js"></script>
 </html>
