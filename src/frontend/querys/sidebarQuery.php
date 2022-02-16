f5cb95b21c9b1b97f594e59959e8513e07e90ed0
Mateo González <95885267+matgon@users.noreply.github.com>
2022-02-16 19:31:46 +0100

    Añadir carpeta de vistas "querys"

---
+++ src/frontend/querys/sidebarQuery.php
@@ -0,0 +1,57 @@
+  <div class="sidebar-logo">
+    FILTROS
+  </div>
+  <ul class="sidebar-navigation">
+    <li class="header">Tipos</li>
+    <li>
+      <a href="../querys/querySociodemographic.php">
+        <i class="fa fa-home" aria-hidden="true"></i> Sociodemográfico
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <i class="fa fa-tachometer" aria-hidden="true"></i> Antecedentes
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <i class="fa fa-tachometer" aria-hidden="true"></i> Clínico-Patológicos
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <i class="fa fa-tachometer" aria-hidden="true"></i> Biopsias Prostáticas
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <i class="fa fa-tachometer" aria-hidden="true"></i> Tras Prostatectomía
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <i class="fa fa-tachometer" aria-hidden="true"></i> Evolutivos
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <i class="fa fa-tachometer" aria-hidden="true"></i> Marcadores
+      </a>
+    </li>
+    <li class="header">Ajustes</li>
+    <li>
+      <a href="#">
+        <i class="fa fa-users" aria-hidden="true"></i> Realizar consulta
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <i class="fa fa-cog" aria-hidden="true"></i> Reiniciar filtros
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <i class="fa fa-info-circle" aria-hidden="true"></i> Exportar consulta
+      </a>
+    </li>
+  </ul>
