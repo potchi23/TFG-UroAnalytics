1e63a87d7a1a35aa41bc927f20b7d6395585f01a
Mateo González <95885267+matgon@users.noreply.github.com>
2022-02-16 19:30:14 +0100

    Update queryIndex.php

--- src/frontend/queryIndex.php
+++ src/frontend/queryIndex.php
@@ -10,11 +10,6 @@
         <title>Realizar consulta</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href="css/sidebar.css">
-        <meta http-equiv="Expires" content="0">
-        <meta http-equiv="Last-Modified" content="0">
-        <meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
-        <meta http-equiv="Pragma" content="no-cache">
-
         <meta charset="utf-8">
     </head>
     <body>  
