e619b64e4cacdcf2fc897328aa60bc9c5c2cacec
Richard Correa <53357059+potchi23@users.noreply.github.com>
2022-02-19 16:34:04 +0100

    Accesibilidad a consultas en navbar

--- src/frontend/common/header.php
+++ src/frontend/common/header.php
@@ -7,7 +7,7 @@
     <a class="navbar-brand" href="../dashboard.php">Icono app</a>
     <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
       <li class="nav-item">
-        <a class="nav-link" href="../dashboard.php">Consultas <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="../queryIndex.php">Consultas <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="../predictions/predictions.php">Predicciones</a>
