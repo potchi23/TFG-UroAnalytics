2eea83f996f2c71c447b463a78d0d31785b43ff3
Laura González Falque <76649387+laurafalque14@users.noreply.github.com>
2022-02-20 20:12:58 +0100

    Update header.php
    ref to patients added

--- src/frontend/common/header.php
+++ src/frontend/common/header.php
@@ -13,7 +13,7 @@
         <a class="nav-link" href="../predictions/predictions.php">Predicciones</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Pacientes</a>
+        <a class="nav-link" href="../patients/patientsIndex.php">Pacientes</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">Datos</a>
