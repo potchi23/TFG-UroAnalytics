5b0c4b98eab84a5aae089dd7e2f647fbb72232a2
marycvr <mariana97villar@gmail.com>
2022-03-19 22:01:40 +0100

    cambio en las consultas

--- src/frontend/common/includes.php
+++ src/frontend/common/includes.php
@@ -2,4 +2,6 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <link rel="stylesheet" href="../css/sidebar.css">
 <link rel="stylesheet" href="../css/header.css">
+<link rel="stylesheet" href="../css/style.css">
 <meta charset="utf-8">
+
--- src/frontend/queryIndex.php
+++ src/frontend/queryIndex.php
@@ -26,6 +26,28 @@
                     field.style.display = "none"
                 }
             }
+
+            function toggleButtons(op){
+                let switches = ["boolCIR", "boolSociodemographic", "boolClinic", "boolBiopsy", "boolProstate", "boolEvolve", "boolMarkers"];
+                let fields = ["queryCIR", "querySociodemographic", "queryClinic", "queryBiopsy", "queryProstate", "queryEvolve", "queryMarkers"];
+                let fieldsSidebar = ["queryCIRSidebar", "querySociodemographicSidebar", "queryClinicSidebar", "queryBiopsySidebar", "queryProstateSidebar", "queryEvolveSidebar", "queryMarkersSidebar"];
+
+                
+                if(op == 'enableAll'){
+                    for(var i=0; i < switches.length; i++){
+                        document.getElementById(switches[i]).checked = true;
+                        toggleField(fields[i], switches[i]);
+                        toggleField(fieldsSidebar[i], switches[i]);
+                    }
+                }
+                else if (op == 'disableAll'){
+                    for(var i=0; i < switches.length; i++){
+                        document.getElementById(switches[i]).checked = false;
+                        toggleField(fields[i], switches[i]);
+                        toggleField(fieldsSidebar[i], switches[i]);
+                    }
+                }
+            }
         </script>
 
         <meta charset="utf-8">
@@ -50,34 +72,51 @@
                         </div>
 
                         <div class="jumbotron">
-                            <div class="custom-control custom-switch">
-                                <input type="checkbox" checked="false" class="custom-control-input" id="boolCIR" onclick="toggleField('queryCIR', 'boolCIR'); toggleField('queryCIRSidebar', 'boolCIR')">
-                                <label class="custom-control-label" for="boolCIR">Filiación</label>
+                            <div class="row">
+                                <div class="col">
+                                    <div class="custom-control custom-switch custom-switch-md">
+                                        <input type="checkbox" class="custom-control-input" id="boolCIR" onclick="toggleField('queryCIR', 'boolCIR'); toggleField('queryCIRSidebar', 'boolCIR')">
+                                        <label class="custom-control-label" for="boolCIR">Filiación</label>
+                                    </div>
+                                    <div class="custom-control custom-switch custom-switch-md">
+                                        <input type="checkbox" class="custom-control-input" id="boolSociodemographic" onclick="toggleField('querySociodemographic', 'boolSociodemographic'); toggleField('querySociodemographicSidebar', 'boolSociodemographic')">
+                                        <label class="custom-control-label" for="boolSociodemographic">Sociodemográfico</label>
+                                    </div>
+                                    <div class="custom-control custom-switch custom-switch-md">
+                                        <input type="checkbox" class="custom-control-input " id="boolClinic" onclick="toggleField('queryClinic', 'boolClinic'); toggleField('queryClinicSidebar', 'boolClinic')">
+                                        <label class="custom-control-label" for="boolClinic">Clínico patológico</label>
+                                    </div>
+                                </div>
+
+                                <div class="col">
+                                    <div class="custom-control custom-switch custom-switch-md">
+                                        <input type="checkbox" class="custom-control-input" id="boolBiopsy" onclick="toggleField('queryBiopsy', 'boolBiopsy'); toggleField('queryBiopsySidebar', 'boolBiopsy')">
+                                        <label class="custom-control-label" for="boolBiopsy">Biopsias prostáticas</label>
+                                    </div>
+                                    <div class="custom-control custom-switch custom-switch-md">
+                                        <input type="checkbox" class="custom-control-input" id="boolProstate" onclick="toggleField('queryProstate', 'boolProstate'); toggleField('queryProstateSidebar', 'boolProstate')">
+                                        <label class="custom-control-label" for="boolProstate">Tras prostatectomía</label>
+                                    </div>
+                                </div>
+
+                                <div class="col">
+                                    <div class="custom-control custom-switch custom-switch-md">
+                                        <input type="checkbox" class="custom-control-input" id="boolEvolve" onclick="toggleField('queryEvolve', 'boolEvolve'); toggleField('queryEvolveSidebar', 'boolEvolve')">
+                                        <label class="custom-control-label" for="boolEvolve">Evolutivos</label>
+                                    </div>
+                                    <div class="custom-control custom-switch custom-switch-md">
+                                        <input type="checkbox" class="custom-control-input" id="boolMarkers" onclick="toggleField('queryMarkers', 'boolMarkers'); toggleField('queryMarkersSidebar', 'boolMarkers')">
+                                        <label class="custom-control-label" for="boolMarkers">Marcadores</label>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="custom-control custom-switch">
-                                <input type="checkbox" class="custom-control-input" id="boolSociodemographic" onclick="toggleField('querySociodemographic', 'boolSociodemographic'); toggleField('querySociodemographicSidebar', 'boolSociodemographic')">
-                                <label class="custom-control-label" for="boolSociodemographic">Sociodemográfico</label>
-                            </div>
-                            <div class="custom-control custom-switch">
-                                <input type="checkbox" class="custom-control-input" id="boolClinic" onclick="toggleField('queryClinic', 'boolClinic'); toggleField('queryClinicSidebar', 'boolClinic')">
-                                <label class="custom-control-label" for="boolClinic">Clínico patoloógico</label>
-                            </div>
-                            <div class="custom-control custom-switch">
-                                <input type="checkbox" class="custom-control-input" id="boolBiopsy" onclick="toggleField('queryBiopsy', 'boolBiopsy'); toggleField('queryBiopsySidebar', 'boolBiopsy')">
-                                <label class="custom-control-label" for="boolBiopsy">Biopsias prostáticas</label>
-                            </div>
-                            <div class="custom-control custom-switch">
-                                <input type="checkbox" class="custom-control-input" id="boolProstate" onclick="toggleField('queryProstate', 'boolProstate'); toggleField('queryProstateSidebar', 'boolProstate')">
-                                <label class="custom-control-label" for="boolProstate">Tras prostatectomía</label>
-                            </div>
-                            <div class="custom-control custom-switch">
-                                <input type="checkbox" class="custom-control-input" id="boolEvolve" onclick="toggleField('queryEvolve', 'boolEvolve'); toggleField('queryEvolveSidebar', 'boolEvolve')">
-                                <label class="custom-control-label" for="boolEvolve">Evolutivos</label>
-                            </div>
-                            <div class="custom-control custom-switch">
-                                <input type="checkbox" class="custom-control-input" id="boolMarkers" onclick="toggleField('queryMarkers', 'boolMarkers'); toggleField('queryMarkersSidebar', 'boolMarkers')">
-                                <label class="custom-control-label" for="boolMarkers">Marcadores</label>
-                            </div>
+
+                            <br>
+
+                            <div class="row">
+                                <button class="btn btn-primary btn-md ml-4" id="enableAll" onclick="toggleButtons('enableAll')">Seleccionar todos</button>
+                                <button class="btn btn-secondary btn-md ml-4" id="disableAll" onclick="toggleButtons('disableAll')">Eliminar filtros</button>
+                            </div>  
                         </div>
                         
                         <div id="queryCIR" style="display:none;">
