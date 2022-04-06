121c3ffa89581d0338cf91e43abbf68e7b79699e
Richard Correa <53357059+potchi23@users.noreply.github.com>
2022-04-06 15:23:40 +0200

    ViewPatient arreglado

--- src/backend/tfg_server.py
+++ src/backend/tfg_server.py
@@ -481,27 +481,6 @@ def buildQuery(req):
 
     return query
 
-
-@app.route('/numPatients', methods=['GET'])
-@token_required
-def numPatients(current_user):
-    status = 401
-    response = {}
-
-    if request.method == 'GET':        
-
-        meta_data = sqla.MetaData(bind=engine)
-        sqla.MetaData.reflect(meta_data)
-        
-        patients_table = meta_data.tables['patients']
-        
-        result = sqla.select([sqla.func.count()]).select_from(patients_table).scalar()
-        
-        response['num_patients'] = result
-        status = 200
-
-        return response, status 
-
 @app.route('/patients', methods=['POST', 'GET'])
 @token_required
 def viewPatients(current_user):
@@ -520,89 +499,26 @@ def viewPatients(current_user):
             'data':[]
         }
 
+        entry = {}
         for request_p in cursor:
-            data = {
-                'N': request_p[0],
-                'FECHACIR':request_p[1],
-                'EDAD':request_p[2],
-                'ETNIA':request_p[3],
-                'OBESO':request_p[4],
-                'HTA':request_p[5],
-                'DM':request_p[6],
-                'TABACO':request_p[7],
-                'HEREDA':request_p[8],
-                'TACTOR':request_p[9],
-                'PSAPRE':request_p[10],
-                'PSALT':request_p[11],
-                'TDUPPRE':request_p[12],
-                'ECOTR':request_p[13],
-                'NBIOPSIA':request_p[14],
-                'HISTO':request_p[15],
-                'GLEASON1':request_p[16],
-                'NCILPOS':request_p[17],
-                'BILAT':request_p[18],
-                'PORCENT':request_p[19],
-                'IPERIN':request_p[20],
-                'ILINF':request_p[21],
-                'IVASCU':request_p[22],
-                'TNM1':request_p[23],
-                'HISTO2':request_p[24],
-                'GLEASON2':request_p[25],
-                'BILAT2':request_p[26],
-                'LOCALIZ':request_p[27],
-                'MULTIFOC':request_p[28],
-                'VOLUMEN':request_p[29],
-                'EXTRACAP':request_p[30],
-                'VVSS':request_p[31],
-                'IPERIN2':request_p[32],
-                'ILINF2':request_p[33],
-                'IVASCU2':request_p[34],
-                'PINAG':request_p[35],
-                'MARGEN':request_p[36],
-                'TNM2':request_p[37],
-                'PSAPOS':request_p[38],
-                'RTPADYU':request_p[39],
-                'RTPMES':request_p[40],
-                'RBQ':request_p[41],
-                'TRBQ':request_p[42],
-                'TDUPLI':request_p[43],
-                'TDUPLI.r1':request_p[44],
-                'T1MTX':request_p[45],
-                'FECHAFIN':request_p[46],
-                't.seg':request_p[47],
-                'FALLEC':request_p[48],
-                'TSUPERV':request_p[49],
-                'TSEGUI':request_p[50],
-                'PSAFIN':request_p[51],
-                'CAPRA-S':request_p[52],
-                'RA-NUCLEAR':request_p[53],
-                'RA-ESTROMA':request_p[54],
-                'PTEN':request_p[55],
-                'ERG':request_p[56],
-                'KI-67':request_p[57],
-                'SPINK1':request_p[58],
-                'C-MYC':request_p[59],
-                'NOTAS':request_p[60],
-                'IMC':request_p[61],
-                'ASA':request_p[62],
-                'GR':request_p[63],
-                'PNV':request_p[64],
-                'TQ':request_p[65],
-                'TH':request_p[66],
-                'NGG':request_p[67],
-                'PGG':request_p[68]
-            }
-            response['data'].append(data)
-        
+            for i in range(0, len(request_p)):
+                value = request_p[i]
+                if value is not None:
+                    entry[cursor.description[i][0]] = request_p[i]
+                else:
+                    entry[cursor.description[i][0]] = 'Sin datos'
+                           
+            response['data'].append(dict(entry))
+    
         query = f'SELECT COUNT(N) FROM patients'
         cursor.execute(query)
         response['num_entries'] = cursor.fetchone()
 
         cursor.close()
-
         status = 200
         
         return response, status
+
     elif  request.method == 'POST':
         FECHACIR = request.form['FECHACIR']
         EDAD = request.form['EDAD']
@@ -672,7 +588,6 @@ def viewPatients(current_user):
         NGG = request.form['NGG']
         PGG = request.form['PGG']
         
-
         cursor = mydb.cursor()
         query = 'INSERT INTO patients(FECHACIR, EDAD, ETNIA, HTA, DM, TABACO, HEREDA, TACTOR, PSAPRE, PSALT, TDUPPRE, ECOTR, NBIOPSIA, HISTO, GLEASON1, NCILPOS, BILAT, PORCENT, IPERIN, ILINF,IVASCU, TNM1, HISTO2, GLEASON2, BILAT2, LOCALIZ, MULTIFOC, VOLUMEN, EXTRACAP, VVSS, IPERIN2, ILINF2, IVASCU2, PINAG, MARGEN, TNM2, PSAPOS, RTPADYU, RTPMES, RBQ, TRBQ, TDUPLI, TDUPLI.r1, T1MTX, FECHAFIN, t.seg, FALLEC, TSUPERV, TSEGUI, PSAFIN, CAPRA S, RA-NUCLEAR, RA-ESTROMA, PTEN, ERG, KI-67, SPINK1, C-MYC, NOTAS, IMC, ASA, GR, PNV, TQ, TH, NGG, PGG) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
         values = (FECHACIR, EDAD, ETNIA, HTA, DM, TABACO, HEREDA, TACTOR, PSAPRE, PSALT, TDUPPRE, ECOTR, NBIOPSIA, HISTO, GLEASON1, NCILPOS, BILAT, PORCENT, IPERIN, ILINF,IVASCU, TNM1, HISTO2, GLEASON2, BILAT2, LOCALIZ, MULTIFOC, VOLUMEN, EXTRACAP, VVSS, IPERIN2, ILINF2, IVASCU2, PINAG, MARGEN, TNM2, PSAPOS, RTPADYU, RTPMES, RBQ, TRBQ, TDUPLI, TDUPLI_r1, T1MTX, FECHAFIN, t_seg, FALLEC, TSUPERV, TSEGUI, PSAFIN, CAPRA_S, RA_nuclear, RA_estroma, PTEN, ERG, KI_67, SPINK1, C_MYC, NOTAS, IMC, ASA, GR, PNV, TQ, TH, NGG, PGG)
--- src/frontend/patients/viewPatient.php
+++ src/frontend/patients/viewPatient.php
@@ -46,12 +46,7 @@
 
                     <?php
                         $http_requests = new HttpRequests();
-                        $response = $http_requests->getResponse("$BACKEND_URL/numPatients", "GET", "", $user->get_token());
                         
-                        $num_patients = 0;
-                        if($response["status"] == 200) {                          
-                            $numPatients = $response["data"]->num_patients;                        
-                        }
                         $page = $_SESSION["page"];
                         
                         $get_req = array(
@@ -71,166 +66,35 @@
                                 header("Location: ../login.php");
                             }
                         }
+                        
+                        $num_patients = $response["data"]->num_entries[0];
 
                         if($num_patients > 0) 
                             echo "<h5>Número de pacientes: " . $num_patients . "</h5><br>";
                         else
                             echo "<h5>No hay pacientes en la base de datos</h5><br>";                                        
                     ?>
-
+                    
                     <div class="table-container m-auto">
                         <div class="table-responsive table-content">
                             <table class="table table-striped table-bordered table-hover table-light">
                                 <?php                                    
                                     if (count($data_array) > 0) {
-                                        echo <<< EOL
-                                            <tr class="thead-dark">
-                                            <th>N</th>
-                                            <th>FECHACIR</th> 
-                                            <th>EDAD</th> 
-                                            <th>ETNIA</th> 
-                                            <th>HTA</th>
-                                            <th>DM</th>
-                                            <th>TABACO</th>
-                                            <th>HEREDA</th>
-                                            <th>TACTOR</th>
-                                            <th>PSAPRE</th>
-                                            <th>PSALT</th>
-                                            <th>TDUPPRE</th>
-                                            <th>ECOTR</th>
-                                            <th>NBIOPSIA</th>
-                                            <th>HISTO</th>
-                                            <th>GLEASON1</th>
-                                            <th>NCILPOS</th>
-                                            <th>BILAT</th>
-                                            <th>PORCENT</th>
-                                            <th>IPERIN</th>
-                                            <th>ILINF</th>
-                                            <th>IVASCU</th>
-                                            <th>TNM1</th>
-                                            <th>HISTO2</th>
-                                            <th>GLEASON2</th>
-                                            <th>BILAT2</th>
-                                            <th>LOCALIZ</th>
-                                            <th>MULTIFOC</th>
-                                            <th>VOLUMEN</th>
-                                            <th>EXTRACAP</th>
-                                            <th>VVSS</th>
-                                            <th>IPERIN2</th>
-                                            <th>ILINF2</th>
-                                            <th>IVASCU2</th>
-                                            <th>PINAG</th>
-                                            <th>MARGEN</th>
-                                            <th>TNM2</th>
-                                            <th>PSAPOS</th>
-                                            <th>RTPADYU</th>
-                                            <th>RTPMES</th>
-                                            <th>RBQ</th>
-                                            <th>TRBQ</th>
-                                            <th>TDUPLI</th>
-                                            <th>TDUPLI</th>
-                                            <th>T1MTX</th>
-                                            <th>FECHAFIN</th>
-                                            <th>t.seg</th>
-                                            <th>FALLEC</th>
-                                            <th>TSUPERV</th>
-                                            <th>TSEGUI</th>
-                                            <th>PSAFIN</th>
-                                            <th>CAPRA S</th>
-                                            <th>RA-NUCLEAR</th>
-                                            <th>RA-ESTROMA</th>
-                                            <th>PTEN</th>
-                                            <th>ERG</th>
-                                            <th>KI-67</th>
-                                            <th>SPINK1</th>
-                                            <th>C-MYC</th>
-                                            <th>NOTAS</th>
-                                            <th>IMC</th>
-                                            <th>ASA</th>
-                                            <th>GR</th>
-                                            <th>PNV</th>
-                                            <th>TQ</th>
-                                            <th>TH</th>
-                                            <th>NGG</th>
-                                            <th>PGG</th>
-                                            </tr>
-                                        EOL;
 
+                                        echo '<tr class="thead-dark">';
+                                        foreach($data_array[0] as $key=>$value){
+                                            echo "<th>$key</th>";
+                                        }
+                                        echo "</tr>";  
+  
                                         $numElems = count($data_array);
 
                                         foreach($data_array as $petition){
-                                            echo <<<EOL
-                                                <tr id="patients_$petition->N">
-                                                <td> $petition->N </td>
-                                                <td> $petition->FECHACIR </td>
-                                                <td> $petition->EDAD </td>
-                                                <td> $petition->ETNIA </td>
-                                                <td> $petition->HTA </td>
-                                                <td> $petition->DM </td>
-                                                <td> $petition->TABACO </td>
-                                                <td> $petition->HEREDA </td>
-                                                <td> $petition->TACTOR </td>
-                                                <td> $petition->PSAPRE </td>
-                                                <td> $petition->PSALT </td>
-                                                <td> $petition->TDUPPRE </td>
-                                                <td> $petition->ECOTR </td>
-                                                <td> $petition->NBIOPSIA </td>
-                                                <td> $petition->HISTO </td>
-                                                <td> $petition->GLEASON1 </td>
-                                                <td> $petition->NCILPOS </td>
-                                                <td> $petition->BILAT </td>
-                                                <td> $petition->PORCENT </td>
-                                                <td> $petition->IPERIN </td>
-                                                <td> $petition->ILINF </td>
-                                                <td> $petition->IVASCU </td>
-                                                <td> $petition->TNM1 </td>
-                                                <td> $petition->HISTO2 </td>
-                                                <td> $petition->GLEASON2 </td>
-                                                <td> $petition->BILAT2 </td>
-                                                <td> $petition->LOCALIZ </td>
-                                                <td> $petition->MULTIFOC </td>
-                                                <td> $petition->VOLUMEN </td>
-                                                <td> $petition->EXTRACAP </td>
-                                                <td> $petition->VVSS </td>
-                                                <td> $petition->IPERIN2 </td>
-                                                <td> $petition->ILINF2 </td>
-                                                <td> $petition->IVASCU2 </td>
-                                                <td> $petition->PINAG </td>
-                                                <td> $petition->MARGEN </td>
-                                                <td> $petition->TNM2 </td>
-                                                <td> $petition->PSAPOS </td>
-                                                <td> $petition->RTPADYU </td>
-                                                <td> $petition->RTPMES </td>
-                                                <td> $petition->RBQ </td>
-                                                <td> $petition->TRBQ </td>
-                                                <td> $petition->TDUPLI </td>
-                                                <td> $petition->TDUPLI </td>
-                                                <td> $petition->T1MTX </td>
-                                                <td> $petition->FECHAFIN </td>
-                                                <td> $petition->t_seg </td>
-                                                <td> $petition->FALLEC </td>
-                                                <td> $petition->TSUPERV </td>
-                                                <td> $petition->TSEGUI </td>
-                                                <td> $petition->PSAFIN </td>
-                                                <td> $petition->CAPRA </td>
-                                                <td> $petition->RA_nuclear </td>
-                                                <td> $petition->RA_estroma </td>
-                                                <td> $petition->PTEN </td>
-                                                <td> $petition->ERG </td>
-                                                <td> $petition->KI </td>
-                                                <td> $petition->SPINK1 </td>
-                                                <td> $petition->C_MYC </td>
-                                                <td> $petition->NOTAS </td>
-                                                <td> $petition->IMC </td>
-                                                <td> $petition->ASA </td>
-                                                <td> $petition->GR </td>
-                                                <td> $petition->PNV </td>
-                                                <td> $petition->TQ </td>
-                                                <td> $petition->TH </td>
-                                                <td> $petition->NGG </td>
-                                                <td> $petition->PGG </td>
-                                                </tr>
-                                            EOL;
+                                            echo "<tr id='patients_$petition->N'>";
+                                            foreach($petition as $key=>$value){
+                                                echo "<td>$value</td>";
+                                            }
+                                            echo "</tr>";                                      
                                         }
 
                                         $empty_rows = $NUM_ELEMENTS_BY_PAGE - $numElems;
@@ -247,34 +111,33 @@
                                         echo "<h3>No existen pacientes en la base de datos</h3>";
                                     }
                                 ?>
-                            </table>
-                            
-                            <div class="page-buttons">
-                                <?php
-                                if(count($data_array) > 0){
-                                    echo "<div>";
-                                    if($_SESSION["page"] != 1 && count($data_array) > 0){
-                                        $prev_page = $_SESSION["page"] - 1;    
-                                        echo "<a class='btn btn-primary previous' href='viewPatient.php?page=$prev_page'><</a>";
-                                    }                                    
-                                    echo "</div>";
-                                    
-                                    $numPages = ceil($response["data"]->num_entries[0]/$NUM_ELEMENTS_BY_PAGE);
-                                    echo "<div style='background-color:#e9ecef; border-color:#e9ecef' class='btn btn-warning'>$page/$numPages</div>";
-
-                                    echo "<div>";
-                                    if ($get_req["offset"] + $NUM_ELEMENTS_BY_PAGE < $response["data"]->num_entries[0] && count($data_array) > 0){
-                                        $next_page = $_SESSION["page"] + 1;    
-                                        echo "<a class='btn btn-primary next' href='viewPatient.php?page=$next_page'>></a>"; 
-                                    }
-                                    else{
-                                        echo "<div style='background-color:#e9ecef; border-color:#e9ecef;'class='btn btn-primary previous'>></div>";      
-                                    }
-                                    echo "</div>";
-                                }
-                                ?>
-                            </div>
+                            </table>                    
                         </div>
+                    </div>
+                    <div class="page-buttons">
+                        <?php
+                        if(count($data_array) > 0){
+                            echo "<div>";
+                            if($_SESSION["page"] != 1 && count($data_array) > 0){
+                                $prev_page = $_SESSION["page"] - 1;    
+                                echo "<a class='btn btn-primary previous' href='patientsIndex.php?page=$prev_page'><</a>";
+                            }                                    
+                            echo "</div>";
+                            
+                            $numPages = ceil($response["data"]->num_entries[0]/$NUM_ELEMENTS_BY_PAGE);
+                            echo "<div style='background-color:#e9ecef; border-color:#e9ecef' class='btn btn-warning'>$page/$numPages</div>";
+
+                            echo "<div>";
+                            if ($get_req["offset"] + $NUM_ELEMENTS_BY_PAGE < $response["data"]->num_entries[0] && count($data_array) > 0){
+                                $next_page = $_SESSION["page"] + 1;    
+                                echo "<a class='btn btn-primary next' href='patientsIndex.php?page=$next_page'>></a>"; 
+                            }
+                            else{
+                                echo "<div style='background-color:#e9ecef; border-color:#e9ecef;'class='btn btn-primary previous'>></div>";      
+                            }
+                            echo "</div>";
+                        }
+                        ?>
                     </div>
                 </div>
             </div>
