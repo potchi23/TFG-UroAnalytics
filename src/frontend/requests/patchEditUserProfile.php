50fc1d654b3049b4b05e7c050337a6ee33b8dcd7
Maryny <72405443+marynyzcc@users.noreply.github.com>
2022-05-19 16:04:44 +0200

    Correción ortografía

--- src/frontend/requests/patchEditUserProfile.php
+++ src/frontend/requests/patchEditUserProfile.php
@@ -16,16 +16,16 @@
     $password = htmlspecialchars($_POST["password"]);
     
     append_error_message(strlen($name) <= 0, "El nombre no puede ser vacío");
-    append_error_message(strlen($name) >= 20, "El nombre no puede tener más de 20 carácteres");
+    append_error_message(strlen($name) >= 20, "El nombre no puede tener más de 20 caracteres");
     append_error_message(strlen($surname_1) <= 0, "El apellido 1 no puede ser vacío");
-    append_error_message(strlen($surname_1) >= 20, "El Apellido 1 no puede tener más de 20 carácteres");
+    append_error_message(strlen($surname_1) >= 20, "El Apellido 1 no puede tener más de 20 caracteres");
     append_error_message(strlen($surname_2) <= 0, "El apellido 2 no puede ser vacío");
-    append_error_message(strlen($surname_2) >= 20, "El Apellido 2 no puede tener más de 20 carácteres");
+    append_error_message(strlen($surname_2) >= 20, "El Apellido 2 no puede tener más de 20 caracteres");
     append_error_message(strlen($email) <= 0, "El email no puede ser vacío");
-    append_error_message(strlen($email) > 50, "El email  no puede tener más de 50 carácteres");
+    append_error_message(strlen($email) > 50, "El email  no puede tener más de 50 caracteres");
     append_error_message(strlen($email) > 0 && strlen($email) <= 50 && !filter_var($email, FILTER_VALIDATE_EMAIL), "$email no es un email válido");
     append_error_message($_POST["password"] != $_POST["password_confirm"], "Las contraseñas no coinciden");
-    append_error_message(strlen($_POST["password"]) > 0 && !preg_match("/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/", $_POST["password"]), "La contraseña debe contener al menos 8 carácteres alfanuméricos con mayúsculas y minúsculas");
+    append_error_message(strlen($_POST["password"]) > 0 && !preg_match("/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/", $_POST["password"]), "La contraseña debe contener al menos 8 caracteres alfanuméricos con mayúsculas y minúsculas");
  
     if (count($_SESSION["error"]) > 0) {
         header("Location: ../userProfile.php");
